a9d78b0b68bb2fbeab048b071d10cbd15b0f56cb
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-07-09 11:43:30 -0700

    fix prettier issue

--- src/services/ProjectsService.ts
+++ src/services/ProjectsService.ts
@@ -8,7 +8,7 @@ export default class ProjectsService implements Service<Project[]> {
       const res = await axios.get<Project[]>(
         "https://api.github.com/users/BenjaminHinchliff/repos?per_page=100"
       );
-      return res.data.map((project) => {
+      return res.data.map(project => {
         project.htmlUrl = project["html_url"] as string;
         delete project["html_url"];
         return project;
