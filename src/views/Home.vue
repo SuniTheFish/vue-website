eb8526de570151d0a6b7e87f2b17e036e25d01eb
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-07-25 20:14:43 -0700

    added portrait to homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,33 @@
 <template>
   <div class="home" key="/">
     <h1>About Me</h1>
-    <p>
-      Hello! I'm
-      <v-tooltip right>
-        <template v-slot:activator="{ on, attrs }">
-          <span class="font-weight-bold" v-bind="attrs" v-on="on">
-            Benjamin,
-          </span>
-        </template>
-        <v-img src="../assets/img/portrait.jpg" width="20vh" height="20vh" />
-      </v-tooltip>
-      a (largely) independent developer who codes in every spare moment I get.
-      This is the website where I post my projects. I love to mess around with
-      whatever seems interesting to me at the moment, and I'm always trying to
-      improve my skills.
-      <span class="text--disabled">
-        (Case in point: this site - the
-        <a
-          href="https://web.archive.org/web/2019*/http://benjaminhinchliff.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          >way back machine</a
-        >
-        for this is pretty interesting (to me))
-      </span>
-    </p>
+    <div class="description d-flex">
+      <v-img
+        class="rounded mr-2"
+        src="../assets/img/portrait.jpg"
+        aspect-ratio="1"
+        height="100"
+        width="100"
+        contain=""
+      />
+      <p>
+        Hello! I'm Benjamin, a (largely) independent developer who codes in
+        every spare moment I get. This is the website where I post my projects.
+        I love to mess around with whatever seems interesting to me at the
+        moment, and I'm always trying to improve my skills.
+        <span class="text--disabled">
+          (Case in point: this site - the
+          <a
+            href="https://web.archive.org/web/2019*/http://benjaminhinchliff.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            way back machine
+          </a>
+          for this is pretty interesting (to me))
+        </span>
+      </p>
+    </div>
     <h1>Projects</h1>
     <v-container>
       <projects-list :num="5" :projectsService="projectsService" />
